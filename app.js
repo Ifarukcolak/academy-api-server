ce4d72976dff646697bd35759f774ef85e42f0f0
Ifarukcolak <ismetfarukcolak@gmail.com>
2023-07-08 21:05:55 +0300

    cors middleware'i eklendş

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 // es5 vs es6
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors')
 const app = express();
 
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '5mb' }));
 
 let sampleList = [
     { 'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'age': 20 },
-    { 'id': 2, 'firstName': 'Faruk', 'lastName': 'Çolak', 'age': 20 },
-    { 'id': 3, 'firstName': 'John', 'lastName': 'Doe', 'age': 20 },
-    { 'id': 4, 'firstName': 'John', 'lastName': 'Doe', 'age': 20 },
-    { 'id': 5, 'firstName': 'Faruk', 'lastName': 'Çolak', 'age': 20 },
-    { 'id': 6, 'firstName': 'John', 'lastName': 'Doe', 'age': 20 },
-    { 'id': 7, 'firstName': 'John', 'lastName': 'Doe', 'age': 20 },
-    { 'id': 8, 'firstName': 'Faruk', 'lastName': 'Çolak', 'age': 20 },
-    { 'id': 9, 'firstName': 'John', 'lastName': 'Doe', 'age': 20 },
+    { 'id': 2, 'firstName': 'Faruk', 'lastName': 'Çolak', 'age': 25 },
+    { 'id': 3, 'firstName': 'John', 'lastName': 'Doe', 'age': 28 },
+    { 'id': 4, 'firstName': 'John', 'lastName': 'Doe', 'age': 27 },
+    { 'id': 5, 'firstName': 'Faruk', 'lastName': 'Çolak', 'age': 32 },
+    { 'id': 6, 'firstName': 'John', 'lastName': 'Doe', 'age': 45 },
+    { 'id': 7, 'firstName': 'John', 'lastName': 'Doe', 'age': 54 },
+    { 'id': 8, 'firstName': 'Faruk', 'lastName': 'Çolak', 'age': 38 },
+    { 'id': 9, 'firstName': 'John', 'lastName': 'Doe', 'age': 12 },
 ];
 
 
 app.get('/', (req, res, next) => {
     res.json(sampleList);
 });
-app.get('/byFilter', (req, res, next) => {
-   const ageLimit = Number(req.headers.age);
-   const filteredList = sampleList.filter(x=> x.age < ageLimit);
 
-    res.json({filteredList}) 
+app.get('/byFilter', (req, res, next) => {
+    const ageLimit = Number(req.headers.age);
+    const filteredList = sampleList.filter(x => x.age < ageLimit);
+    res.json({ filteredList })
 });
+
 app.get('/:id', (req, res, next) => {
     let result = sampleList.find(x => Number(x.id) === Number(req.params.id));
     res.json(result);
 });
-
-
 
 app.post('/', (req, res, next) => {
     let element = req.body;
@@ -44,44 +44,43 @@ app.post('/', (req, res, next) => {
 
         element.id = length + 1;
         sampleList.push(element);
-        res.json({ 'message': 'Eleman eklendi.' });
+        res.json({ 'message': 'Eleman eklendi.', 'id': element.id });
     } else {
         res.json({ 'message': 'Eleman kısıtlara uygun değil.' });
     }
 });
 
 app.post('/multiple', (req, res, next) => {
-    
-        const elements = req.body;
-   
-     if (Array.isArray(elements) && elements.length > 0) {
-       const addedElements = [];
-   
-       elements.forEach((element, index) => {
-         if (element.firstName && element.lastName && element.age) {
-           const length = sampleList.length;
-   
-           const newElement = {
-             id: length + index + 1,
-             firstName: element.firstName,
-             lastName: element.lastName, 
-             age: element.age
-           };
-   
-           sampleList.push(newElement);
-           addedElements.push(newElement);
-         }
-       });
-   
-       if (addedElements.length > 0) {
-         res.json({ 'message': 'Elemanlar eklendi.', 'addedElements': addedElements });
-       } else {
-         res.json({ 'message': 'Hiçbir eleman kısıtlara uygun değil.' });
-       }
-     } else {
-       res.json({ 'message': 'Geçersiz veri formatı veya boş liste.' });
-     }
-   });
+    const elements = req.body;
+
+    if (Array.isArray(elements) && elements.length > 0) {
+        const addedElements = [];
+
+        elements.forEach((element, index) => {
+            if (element.firstName && element.lastName && element.age) {
+                const length = sampleList.length;
+
+                const newElement = {
+                    id: length + 1,
+                    firstName: element.firstName,
+                    lastName: element.lastName,
+                    age: element.age
+                };
+
+                sampleList.push(newElement);
+                addedElements.push(newElement);
+            }
+        });
+
+        if (addedElements.length > 0) {
+            res.json({ 'message': 'Elemanlar eklendi.', addedElements });
+        } else {
+            res.json({ 'message': 'Hiçbir eleman kısıtlara uygun değil.' });
+        }
+    } else {
+        res.json({ 'message': 'Geçersiz veri formatı veya boş liste.' });
+    }
+});
 
 
 app.put('/:id', (req, res, next) => {
@@ -97,7 +96,7 @@ app.put('/:id', (req, res, next) => {
             }
         });
 
-        res.json({ 'message': 'Eleman güncellendi' });
+        res.json({ 'message': 'Eleman güncellendi', 'id': id });
     } else {
         res.json({ 'message': 'Eleman bulunamadı!' });
     }
@@ -109,7 +108,7 @@ app.delete('/:id', (req, res, next) => {
 
     if (index > -1) {
         sampleList.splice(index, 1);
-        res.json({ 'message': 'Eleman silindi!' });
+        res.json({ 'message': 'Eleman silindi!', id });
     } else {
         res.json({ 'message': 'Eleman bulunamadı!' });
     }
